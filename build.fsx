1c423980b9e5f11849d1e75db54f6d6a59ad5e0d
Andrew Cherry <andrew@xyncro.com>
2015-03-11 15:12:29 +0000

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -30,7 +30,7 @@ Target "Publish" (fun _ ->
         { p with
               Authors = [ "Andrew Cherry" ]
               Project = "Chiron"
-              Version = "0.1.8-alpha"
+              Version = "0.1.9-alpha"
               OutputPath = "bin"
               AccessKey = getBuildParamOrDefault "nuget_key" ""
               Publish = hasBuildParam "nuget_key"
