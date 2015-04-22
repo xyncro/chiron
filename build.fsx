185fa616d86a82f638702ba96ea6efd41de01cb3
kolektiv <andrew@xyncro.com>
2015-04-22 20:20:17 +0100

    version inc

--- build.fsx
+++ build.fsx
@@ -33,7 +33,7 @@ Target "Publish" (fun _ ->
                   "Michael Newton"
                   "Henrik Feldt" ]
               Project = "Chiron"
-              Version = "2.0.1"
+              Version = "3.0.0"
               OutputPath = "bin"
               AccessKey = getBuildParamOrDefault "nuget_key" ""
               Publish = hasBuildParam "nuget_key"
