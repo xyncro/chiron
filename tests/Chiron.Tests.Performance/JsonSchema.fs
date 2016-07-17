9f9b50b5a9ec1590cacf039995a3b86ccedcf921
Marcus Griep <marcus@griep.us>
2016-07-17 19:22:27 -0400

    Add encoding benchmarks, `JsonRegex`

--- tests/Chiron.Tests.Performance/JsonSchema.fs
+++ tests/Chiron.Tests.Performance/JsonSchema.fs
@@ -2,6 +2,8 @@ module JsonSchema
 
 open Chiron
 
+let inline flip f x y = f y x
+
 let tryParseJsonToUri str =
     match System.Uri.TryCreate (str, System.UriKind.RelativeOrAbsolute) with
     | true, uri -> Json.init uri
@@ -26,6 +28,47 @@ module Json =
     let inline readMixin json =
         (^a : (static member FromJson: ^a -> Json<'a>) Unchecked.defaultof<_>) json
 
+    let inline writeMixin x json =
+        (^a : (static member ToJson: ^a -> Json<unit>) x) json
+
+type BoundaryType = Inclusive | Exclusive
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module BoundaryType =
+    let ofBool = function
+    | true -> Exclusive
+    | false -> Inclusive
+
+type NumericBoundary =
+    { limit : decimal
+      boundaryType : BoundaryType }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module NumericBoundary =
+    let tryMake limit : Json -> JsonResult<NumericBoundary> =
+        Unchecked.defaultof<_>
+
+type JsonRegex =
+    { originalString : string
+      regex : System.Text.RegularExpressions.Regex }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module JsonRegex =
+    let tryMake (str : string) : Choice<JsonRegex,string> =
+        try
+            let regex = System.Text.RegularExpressions.Regex (str, System.Text.RegularExpressions.RegexOptions.ECMAScript)
+            { originalString = str; regex = regex }
+            |> Choice1Of2
+        with
+        | :? System.ArgumentException as e ->
+            Choice2Of2 <| "Error converting pattern to regular expression: " + e.Message            
+    let decode : Json -> JsonResult<JsonRegex> = function
+        | Json.String str ->
+            tryMake str |> function Choice1Of2 v -> JsonResult.Value v | Choice2Of2 e -> JsonResult.Error e
+        | json -> JsonResult.Error ("Expected a JSON string, but found: " + Json.format json)
+    let encode (jregex : JsonRegex) : Json =
+        Json.String jregex.originalString
+
 type PropertyName = string
 type JsonSchemaDefinition =
     { id : System.Uri option
@@ -40,7 +83,7 @@ type JsonSchemaDefinition =
       exclusiveMinimum: bool
       maxLength: uint64 option
       minLength: uint64
-      pattern: System.Text.RegularExpressions.Regex option
+      pattern: JsonRegex option
       additionalItems: BoolOrSchema
       items : SchemaOrSchemaArray
       maxItems : uint64 option
@@ -79,8 +122,7 @@ type JsonSchemaDefinition =
         let exclusiveMinimum = defaultArg exclusiveMinimumOpt false
         let! maxLength = Json.readOrDefault "maxLength" None
         let! minLength = Json.readOrDefault "minLength" 0UL
-        let! patternStr = Json.readOrDefault "pattern" None
-        let! pattern = ifProvided tryParseJsonToRegex patternStr
+        let! (pattern : JsonRegex option) = Json.readWithOrDefault (FromJsonDefaults.FromJson Unchecked.defaultof<Json option> |> flip Json.bind (fun (json : Json option) (j : Json) -> match json with None -> JsonResult.Value None, j | Some js -> match JsonRegex.decode js with JsonResult.Value v -> JsonResult.Value (Some v), j | JsonResult.Error err -> JsonResult.Error err, j) >> fst) "pattern" None
         let! additionalItems = Json.readOrDefault "additionalItems" (ElementSchema EmptySchema)
         let! items = Json.readOrDefault "items" (SingleSchema EmptySchema)
         let! maxItems = Json.readOrDefault "maxItems" None
@@ -134,6 +176,40 @@ type JsonSchemaDefinition =
               oneOf = oneOf
               not = not }
     }
+    static member ToJson (x:JsonSchemaDefinition) : Json<unit> = json {
+        do! Json.writeUnlessDefault "id" None (Option.map string x.id)
+        do! Json.writeUnlessDefault "$schema" None (Option.map string x.``$schema``)
+        do! Json.writeUnlessDefault "title" None x.title
+        do! Json.writeUnlessDefault "description" None x.description
+        do! Json.writeUnlessDefault "default" None x.``default``
+        do! Json.writeUnlessDefault "multipleOf" None x.multipleOf
+        do! Json.writeUnlessDefault "maximum" None x.maximum
+        do! Json.writeUnlessDefault "exculsiveMaximum" false x.exclusiveMaximum
+        do! Json.writeUnlessDefault "minimum" None x.minimum
+        do! Json.writeUnlessDefault "exclusiveMinimum" false x.exclusiveMinimum
+        do! Json.writeUnlessDefault "maxLength" None x.maxLength
+        do! Json.writeUnlessDefault "minLength" 0UL x.minLength
+        do! Json.writeWithUnlessDefault (function Some v -> JsonRegex.encode v | None -> Json.Null ()) "pattern" None x.pattern
+        do! Json.writeUnlessDefault "additionalItems" (ElementSchema EmptySchema) x.additionalItems
+        do! Json.writeUnlessDefault "items" (SingleSchema EmptySchema) x.items
+        do! Json.writeUnlessDefault "maxItems" None x.maxItems
+        do! Json.writeUnlessDefault "minItems" 0UL x.minItems
+        do! Json.writeUnlessDefault "uniqueItems" false x.uniqueItems
+        do! Json.writeUnlessDefault "maxProperties" None x.maxProperties
+        do! Json.writeUnlessDefault "minProperties" 0UL x.minProperties
+        do! Json.writeUnlessDefault "required" None x.required
+        do! Json.writeUnlessDefault "additionalProperties" (ElementSchema EmptySchema) x.additionalProperties
+        do! Json.writeUnlessDefault "definitions" Map.empty x.definitions
+        do! Json.writeUnlessDefault "properties" Map.empty x.properties
+        do! Json.writeUnlessDefault "patternProperties" Map.empty x.patternProperties
+        do! Json.writeUnlessDefault "dependencies" None x.dependencies
+        do! Json.writeUnlessDefault "enum" None x.enum
+        do! Json.writeUnlessDefault "type" None x.``type``
+        do! Json.writeUnlessDefault "allOf" None x.allOf
+        do! Json.writeUnlessDefault "anyOf" None x.anyOf
+        do! Json.writeUnlessDefault "oneOf" None x.oneOf
+        do! Json.writeUnlessDefault "not" None x.not
+    }
 and BoolOrSchema =
     | Allowed of bool
     | ElementSchema of JsonSchema
@@ -142,6 +218,10 @@ and BoolOrSchema =
         | Json.Bool b -> Json.init (Allowed b) json
         | Json.Object _ -> (Json.readMixin |> Json.map ElementSchema) json
         | _ -> Json.error "Expected either a boolean or a schema object" json
+    static member ToJson (x:BoolOrSchema) : Json<unit> =
+        match x with
+        | Allowed b -> ToJsonDefaults.ToJson b
+        | ElementSchema s -> Json.writeMixin s
 and SchemaOrSchemaArray =
     | SingleSchema of JsonSchema
     | MultipleSchemas of JsonSchema list
@@ -150,6 +230,10 @@ and SchemaOrSchemaArray =
         | Json.Object _ -> (Json.readMixin |> Json.map SingleSchema) json
         | Json.Array _ -> (FromJsonDefaults.FromJson Unchecked.defaultof<JsonSchema list> |> Json.map MultipleSchemas) json
         | _ -> Json.error "Expected either a schema object or a list of schema objects" json
+    static member ToJson (x:SchemaOrSchemaArray) : Json<unit> =
+        match x with
+        | SingleSchema s -> Json.writeMixin s
+        | MultipleSchemas ms -> ToJsonDefaults.ToJson ms
 and SchemaOrPropertyNames =
     | DependentSchema of JsonSchema
     | DependentProperties of PropertyName list
@@ -158,6 +242,10 @@ and SchemaOrPropertyNames =
         | Json.Object _ -> (Json.readMixin |> Json.map DependentSchema) json
         | Json.Array _ ->  (FromJsonDefaults.FromJson Unchecked.defaultof<PropertyName list> |> Json.map DependentProperties) json
         | _ -> Json.error "Expected either a schema object or an array of property names" json
+    static member ToJson (x:SchemaOrPropertyNames) : Json<unit> =
+        match x with
+        | DependentSchema s -> Json.writeMixin s
+        | DependentProperties ps -> ToJsonDefaults.ToJson ps
 and JsonType =
     | JsonObject
     | JsonArray
@@ -179,6 +267,16 @@ and JsonType =
             | "null" -> Json.init JsonNull
             | _ -> Json.error ("Invalid JSON type: " + v)
     }
+    static member ToJson (x:JsonType) : Json<unit> =
+        match x with
+        | JsonObject -> "object"
+        | JsonArray -> "array"
+        | JsonNumber -> "number"
+        | JsonInteger -> "integer"
+        | JsonString -> "string"
+        | JsonBoolean -> "boolean"
+        | JsonNull -> "null"
+        |> Json.Optic.set Json.String_
 and JsonTypeOrTypes =
     | SingleType of JsonType
     | MultipleTypes of JsonType list
@@ -187,6 +285,10 @@ and JsonTypeOrTypes =
         | Json.String _ -> (Json.readMixin |> Json.map SingleType) json
         | Json.Array _ -> (FromJsonDefaults.FromJson Unchecked.defaultof<JsonType list> |> Json.map MultipleTypes) json
         | _ -> Json.error "Expected either a JSON type or a list of JSON types" json
+    static member ToJson (x:JsonTypeOrTypes) : Json<unit> =
+        match x with
+        | SingleType jt -> Json.writeMixin jt
+        | MultipleTypes mt -> ToJsonDefaults.ToJson mt
 and JsonSchema =
     | EmptySchema
     | SchemaReference of System.Uri
@@ -203,6 +305,11 @@ and JsonSchema =
             let! s = Json.readMixin
             return Schema s
     }
+    static member ToJson (x:JsonSchema) : Json<unit> =
+        match x with
+        | EmptySchema -> Json.init ()
+        | SchemaReference uri -> Json.write "$ref" (string uri)
+        | Schema sd -> Json.writeMixin sd
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module JsonSchemaDefinition =
@@ -222,15 +329,21 @@ module JsonSchemaDefinition =
           dependencies = None; enum = None; ``type`` = None
           allOf = None; anyOf = None; oneOf = None; not = None }
 
-let jsonSchema =
+let jsonSchemaStr : string =
     let assembly = System.Reflection.Assembly.GetExecutingAssembly()
     let resource = "json-schema.json"
     use stream = assembly.GetManifestResourceStream resource
     use reader = new System.IO.StreamReader(stream)
     reader.ReadToEnd()
 
-let jsonSchemaJson =
-    Json.parse jsonSchema
+let jsonSchemaBytes : byte array =
+    System.Text.Encoding.UTF8.GetBytes jsonSchemaStr
+
+let jsonSchemaJson : Json =
+    Json.parse jsonSchemaStr
+
+let jsonSchema : JsonSchema =
+    Json.deserialize jsonSchemaJson
 
 open NBench
 open NBench.Util
@@ -250,7 +363,7 @@ type Parse() =
         TestMode = TestMode.Measurement)>]
     [<CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 1000.)>]
     member __.Benchmark (ctx : BenchmarkContext) : unit =
-        Json.tryParse jsonSchema |> ignore
+        Json.tryParse jsonSchemaStr |> ignore
         incr counter
 
 type Deserialize() =
@@ -269,4 +382,40 @@ type Deserialize() =
     [<CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 1000.)>]
     member __.Benchmark (ctx : BenchmarkContext) : unit =
         (Json.tryDeserialize jsonSchemaJson : Choice<JsonSchema,_>) |> ignore
+        incr counter
+
+type Serialize() =
+    let mutable counter : Counter = Unchecked.defaultof<_>
+
+    [<PerfSetup>]
+    member __.Setup (ctx : BenchmarkContext) : unit =
+        counter <- getCounter ctx "TestCounter"
+
+    [<PerfBenchmark(
+        Description = "Serialize",
+        NumberOfIterations = 10,
+        RunMode = RunMode.Throughput,
+        RunTimeMilliseconds = 1000,
+        TestMode = TestMode.Measurement)>]
+    [<CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 1000.)>]
+    member __.Benchmark (ctx : BenchmarkContext) : unit =
+        Json.serialize jsonSchema |> ignore
+        incr counter
+
+type Format() =
+    let mutable counter : Counter = Unchecked.defaultof<_>
+
+    [<PerfSetup>]
+    member __.Setup (ctx : BenchmarkContext) : unit =
+        counter <- getCounter ctx "TestCounter"
+
+    [<PerfBenchmark(
+        Description = "Format",
+        NumberOfIterations = 10,
+        RunMode = RunMode.Throughput,
+        RunTimeMilliseconds = 1000,
+        TestMode = TestMode.Measurement)>]
+    [<CounterThroughputAssertion("TestCounter", MustBe.GreaterThan, 1000.)>]
+    member __.Benchmark (ctx : BenchmarkContext) : unit =
+        Json.format jsonSchemaJson |> ignore
         incr counter
